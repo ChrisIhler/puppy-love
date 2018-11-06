13ff46cdb914658b9f16672c9f461c8860185449
Sarah Lee <sarahselee223@gmail.com>
2018-11-05 19:56:33 -0800

    change font and add emoji

--- index/index.js
+++ index/index.js
@@ -3,10 +3,10 @@ function emailValidation (email) {
     const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if(filter.test(email.value)){
         alert('Thank you for joining us!')
-        const savedEmail = email.value
         email.value = ''
     } else {
         alert('Please submit a valid email address.')
+        email.value = ''
         return false;
     }
 }
